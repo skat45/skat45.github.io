7d4609076be1ad5bf8990e6ce153f5415aea735b
Andrey Bessonov <88341253+skat45@users.noreply.github.com>
2022-08-13 11:22:03 +0500

    Update visit_card.js

--- JS/visit_card.js
+++ JS/visit_card.js
@@ -11,7 +11,7 @@ function age() {
 	hue += 0.7;
 	if (hue > 360) hue = 0;
 	$('#age').css('color', 'hsl(' + hue + ', 50%, 30%)');
-	let birth = new Date(2003, 10, 10, 12, 0, 0, 0); // 10.11.2003
+	let birth = new Date(2003, 10, 10, 13, 55, 0, 0); // 10.11.2003
 	let now = new Date();
 	let age_now = now.getTime() - birth.getTime();
 	$('#age').html(age_now);
